f5636ea5579e0460c845f5d66438c64bc26ad57d
jozeffff <55282319+jozeffff@users.noreply.github.com>
2021-09-14 04:54:23 +0200

    tried to add enemy movement

--- Planet Game/Assets/Scripts/enemy_fox.cs
+++ Planet Game/Assets/Scripts/enemy_fox.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class enemy_fox : MonoBehaviour
 {   
     public Transform player;
-    private float speed = 0.4f;
+    private float speed = 8;
+    bool finished = false;
+    Vector3 offset;
+    private Vector2 target;
+    private Vector2 position;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {   transform.up = -(player.position - transform.position);
-        // transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
-        // if(transform.position.z <= -10){
-        //     float RNG = Random.Range(-30f , 30f);
-        //     transform.position = new Vector3(RNG , 0 , 0);
+    {   
+        transform.up = -(player.position - transform.position);
+        target = new Vector2(player.position.x, player.position.y + 10);
+        // if (Vector2.Distance(player.position,transform.position)< 10){
+        //     Vector3 up = new Vector3(player.position.x,1.0f,player.position.z);
+        //     this.transform.Translate( up * speed * Time.deltaTime);
+        // }else if (Vector2.Distance(player.position,transform.position)> 10){
+        //     Vector3 down = new Vector3(player.position.x,-1.0f,player.position.z);
+        //     this.transform.Translate(down * speed * Time.deltaTime);
         // }
+        float step = speed * Time.deltaTime;
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+        // float d = Vector2.Distance(player.position,transform.position);
+        // transform.RotateAround(player.position, Vector3.back, speed / (d * 0.2f) * Time.deltaTime);
+
     }
 }
