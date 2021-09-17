6f5acbb71dfc54f351d7d99a57e3ea78bb5aed96
jozef <faithjoe.fj@gmail.com>
2021-09-17 21:38:21 +0200

    OOB coin object spawns, now gets inbound

--- Planet Game/Assets/Scripts/object_collision.cs
+++ Planet Game/Assets/Scripts/object_collision.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class object_collision : MonoBehaviour
-{
+{   
+    // bool in_zone = false;
+    public GameObject player;
+    public GameObject mainPlanetObj;
+    public float speed = 8;
+
     void OnTriggerEnter2D(Collider2D collision)
-    {
+    {   
+
         if (collision.gameObject.tag == "Player" && this.gameObject.tag == "Coin")
         {   
             float temp = GameManager.Instance.get_coin();
@@ -20,6 +26,52 @@ public class object_collision : MonoBehaviour
             GameManager.Instance.set_special(temp);
             Destroy(this.gameObject);
         }
+    }
+        
+    IEnumerator go_in_zone()
+    {   
+        yield return new WaitForSeconds(0.5f);
+        // float d_world_border = mainPlanetObj.GetComponent<PlanetScript>().fullKillBorderDistance;
+        float half_d_world_border = mainPlanetObj.GetComponent<PlanetScript>().fullKillBorderDistance * 0.5f;
+        float d_world_thisObj = Vector2.Distance(mainPlanetObj.transform.position , this.transform.position);
+        // Debug.Log("world distance " + half_d_world_border + " name " + this.name + " dword : "+ d_world_thisObj);
+        if(d_world_thisObj >= half_d_world_border){
+            Vector2 target_position = player.transform.position;
+            while (Vector2.Distance(target_position, this.transform.position) > 0)
+            {
+                float step = speed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, target_position, step);
+                //Wait for a frame to give Unity and other scripts chance to run
+                yield return null;
+            }   
+        }
+    }
+
+
+    void Start(){
+        if (!mainPlanetObj)
+        {
+            GameObject closestPlanet = null;
+            float smallestDistance = Mathf.Infinity;
+
+            foreach (GameObject planet in GameManager.Instance.planets)
+            {
+                float distance = planet.GetComponent<PlanetScript>().calcDistance(gameObject, false);
+                if (distance < smallestDistance)
+                {
+                    closestPlanet = planet;
+                    smallestDistance = distance;
+                }
+            }
+
+            mainPlanetObj = closestPlanet;
+        }
+        if (!player){
+            player = GameManager.Instance.player;
+        }
+
+        StartCoroutine(go_in_zone());
+
     }
 
 }
