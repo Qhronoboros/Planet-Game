28f5afe6311379ccbedcfb47e63ef21f499a4f0e
jozeffff <faithjoe.fj@gmail.com>
2021-09-24 22:07:18 +0200

    working on breadgame( verplaatsen naar desktop)
    save progress to work on desktop

--- Planet Game/Assets/Scripts/BreadshipController.cs
+++ Planet Game/Assets/Scripts/BreadshipController.cs
@@ -5,13 +5,14 @@ using UnityEngine.InputSystem;
 
 public class BreadshipController : MonoBehaviour
 {
-    public GameObject planetObj;
+    // public GameObject planetObj;
     public GameObject projectilePrefab;
 
     public float movementSpeed = 0f;
-    public float maxMovementSpeed = 10.0f;
-    public float accMovementSpeed = 10.0f;
-    public float decMovementSpeed = 10.0f;
+    // public float maxMovementSpeed = 1.0f;
+    // public float accMovementSpeed = 1.0f;
+    // public float decMovementSpeed = 1.0f;
+    Vector2 direction;
     public float shootDelay = 0.2f;
 
     static PlayerController.MovementOptions currentMovement = PlayerController.MovementOptions.Default;
@@ -22,24 +23,38 @@ public class BreadshipController : MonoBehaviour
 
     public void OnMovement(InputAction.CallbackContext value)
     {
-        float joystickMovement = value.ReadValue<Vector2>().x;
-
-        if (joystickMovement < 0)
+        Vector2 joystickMovement = value.ReadValue<Vector2>();
+        
+        if (joystickMovement.x < 0)
         {
             Debug.Log("holdLeft True");
             currentMovement = PlayerController.MovementOptions.Left;
+            
 
         }
-        else if (joystickMovement > 0)
+        else if (joystickMovement.x > 0)
         {
             Debug.Log("holdRight True");
             currentMovement = PlayerController.MovementOptions.Right;
+
         }
+        // else if (joystickMovement.y > 0)
+        // {
+        //     Debug.Log("holdUp True");
+        //     currentMovement = PlayerController.MovementOptions.Up;
+
+        // }else if (joystickMovement.y < 0)
+        // {
+        //     Debug.Log("holdDown True");
+        //     currentMovement = PlayerController.MovementOptions.Down;
+        // }
         else
         {
             currentMovement = PlayerController.MovementOptions.Default;
             Debug.Log("neutral");
         }
+        direction = joystickMovement;
+        print(direction);
     }
 
     public void OnShoot(InputAction.CallbackContext value)
@@ -66,49 +81,24 @@ public class BreadshipController : MonoBehaviour
         if (holdShoot && Time.time - timeLastProjectile > shootDelay)
         {
             GameObject laser = Instantiate(projectilePrefab, transform.position, transform.rotation);
-            laser.GetComponent<ProjectileController>().owner = planetObj.tag;
+            laser.GetComponent<ProjectileController>().owner = this.tag;
 
             timeLastProjectile = Time.time;
         }
 
-        if (currentMovement == PlayerController.MovementOptions.Left && movementSpeed > -maxMovementSpeed)
+        if (currentMovement == PlayerController.MovementOptions.Left )
         {
-            if (movementSpeed > 0.1f)
-            {
-                movementSpeed -= decMovementSpeed * 2 * Time.deltaTime;
-            }
-            else
-            {
-                movementSpeed -= accMovementSpeed * Time.deltaTime;
-            }
+            transform.Translate(direction * movementSpeed * Time.deltaTime);
         }
-        else if (currentMovement == PlayerController.MovementOptions.Right && movementSpeed < maxMovementSpeed)
+        else if (currentMovement == PlayerController.MovementOptions.Right)
         {
-            if (movementSpeed < -0.1f)
-            {
-                movementSpeed += decMovementSpeed * 2 * Time.deltaTime;
-            }
-            else
-            {
-                movementSpeed += accMovementSpeed * Time.deltaTime;
-            }
+            transform.Translate(direction * movementSpeed * Time.deltaTime);
         }
         else
         {
-            if (movementSpeed > 0.1f)
-            {
-                movementSpeed -= decMovementSpeed * 2 * Time.deltaTime;
-            }
-            else if (movementSpeed < -0.1f)
-            {
-                movementSpeed += decMovementSpeed * 2 * Time.deltaTime;
-            }
-            else
-            {
-                movementSpeed = 0;
-            }
+            movementSpeed = 0;
         }
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(movementSpeed, 0), ForceMode2D.Force);
-        //transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        // GetComponent<Rigidbody2D>().AddForce(new Vector2(movementSpeed, 0), ForceMode2D.Force);
+        // transform.Translate(direction * movementSpeed * Time.deltaTime);
     }
 }
