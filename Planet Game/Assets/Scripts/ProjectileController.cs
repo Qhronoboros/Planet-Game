e757c803308c201962aaf6cef8f0b944d1dcae7e
jozeffff <55282319+jozeffff@users.noreply.github.com>
2021-10-06 05:53:40 +0200

    more enemies

--- Planet Game/Assets/Scripts/ProjectileController.cs
+++ Planet Game/Assets/Scripts/ProjectileController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ProjectileController : MonoBehaviour
 {
     public string owner;
+    public string type;
     public Vector2 aimDirection = new Vector2(0, 0);
     public float lifeTime = 30.0f;
     public float speed = 0.3f;
@@ -34,8 +35,16 @@ public class ProjectileController : MonoBehaviour
             Destroy(this.gameObject);
         }
         else if (other.tag == "Enemy" && owner != "Enemy")
-        {
-            other.GetComponent<enemy_fox>().OnHit();
+        {   
+            if (other.GetComponent<enemy_fox>() != null){
+                other.GetComponent<enemy_fox>().OnHit();
+            }
+            else if (other.GetComponent<enemy_dog>() != null){
+                other.GetComponent<enemy_dog>().OnHit();
+            }else if (other.GetComponent<enemy_cat>() != null){
+                other.GetComponent<enemy_cat>().OnHit();
+            }
+
             Destroy(this.gameObject);
         }
         else if (other.tag == "Player" && owner != "Player" && !GameManager.Instance.player.GetComponent<PlayerController>().invincibility)
